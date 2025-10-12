51178e561881f7fdf90dfbb8ec981316dfbcbe8f
INC-Com739 <Iamisaiahcleveland@gmail.com>
2025-10-12 16:28:09 -0500

    Primsa finished

---
+++ prisma/seed.js
@@ -0,0 +1,26 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+app.get("/memes", async (req, res) => {
+  const memes = await prisma.meme.findMany({ include: { user: true } });
+  res.json(memes);
+});
+
+
+async function main() {
+  const user = await prisma.user.create({
+    data: { username: "alice", password: "pass1" }
+  });
+
+  await prisma.meme.create({
+    data: {
+      title: "Distracted Boyfriend",
+      url: "https://i.imgur.com/example1.jpg",
+      userId: user.id
+    }
+  });
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(e => { console.error(e); prisma.$disconnect(); });
